3528239dac13261614941777b10ca1fa5cd26ef0
PastaCodes <marco.j.buda@gmail.com>
2026-05-31 18:54:38 +0200

    fix: use println instead of IO.println

--- reactive/src/Test.scala
+++ reactive/src/Test.scala
@@ -10,21 +10,21 @@ private def printReport(report: Report): Unit =
   def formatBand(band: SizeBand): String = band match
     case SizeBand.Bounded(lower, upper) => s"[$lower, $upper]"
     case SizeBand.BoundedBelow(lower) => s"[$lower, ...]"
-  IO.println(s"Reporting a total of ${report.fileCount} files:")
+  println(s"Reporting a total of ${report.fileCount} files:")
   for (band, count) <- report.sizeDistribution do
-    IO.println(s"${formatBand(band)}: $count files.")
-  IO.println("End of report.")
+    println(s"${formatBand(band)}: $count files.")
+  println("End of report.")
 
 
 @main
 def main(): Unit =
   val startingDirectory = Path.of("/") // automatically maps to something like C:\ on Windows
-  IO.println(s"Scanning \"${startingDirectory.toAbsolutePath}\" for 5 seconds, reporting every 2 seconds...")
+  println(s"Scanning \"${startingDirectory.toAbsolutePath}\" for 5 seconds, reporting every 2 seconds...")
   val stopper = CompletableSubject.create()
   Thread(() =>
     Thread.sleep(5000)
     if !stopper.hasComplete then
-      IO.println("Stopping early. Will now show the last report.")
+      println("Stopping early. Will now show the last report.")
       stopper.onComplete()
   ).start()
   FSStatLib.getFSReportInteractive(startingDirectory, 100_000, 10, stopper)
@@ -32,5 +32,5 @@ def main(): Unit =
     .onBackpressureDrop()
     .blockingSubscribe(
       r => printReport(r),
-      e => IO.println(s"Failed to access the starting directory: $e")
+      e => println(s"Failed to access the starting directory: $e")
     )
